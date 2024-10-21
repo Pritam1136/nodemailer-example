733374b7d36e41b716ecccef0774adc1e39b5b11
Pritam Roy <pritamroy1136@gmail.com>
2024-10-21 16:34:41 +0530

    changed email content

--- server/Handler.js
+++ server/Handler.js
@@ -55,76 +55,63 @@ const sendOtp = (req, res) => {
         <p style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">As Diwali approaches, let's celebrate together at the office by honoring our rich culture and creating lasting memories! 
         Here's what we have planned:${otp}</p><br/>
         <ol>
-        <li style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">Traditional/Cultural Attire Day (30/10/2024): Wear your traditional outfits, like sarees, kurtas, or attire from your culture, and bring Diwali's vibrant spirit to the workplace.</li>
-        <li style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">Workspace Decoration: Get creative! Decorate your desks with colourful rangoli, lights, and traditional Diwali items.</li>
-        <li style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">Diwali Treats & Sweets: Enjoy delicious Diwali snacks and sweets.</li>
-        <li style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">Diwali Gift: A surprise gift for everyone to spread joy and celebrate together.</li>
+          <li style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">Traditional/Cultural Attire Day (30/10/2024): Wear your traditional outfits, like sarees, kurtas, or attire from your culture, and bring Diwali's vibrant spirit to the workplace.</li>
+          <li style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">Workspace Decoration: Get creative! Decorate your desks with colourful rangoli, lights, and traditional Diwali items.</li>
+          <li style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">Diwali Treats & Sweets: Enjoy delicious Diwali snacks and sweets.</li>
+          <li style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">Diwali Gift: A surprise gift for everyone to spread joy and celebrate together.</li>
         </ol>
         <br/>
         <p style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">We hope this celebration brings us closer and helps us appreciate our cultural richness. Please mark your calendars for 30/10/24 and join in the festivities. If you have questions or suggestions, feel free to reach out.</p>
         <br/>
-        <p style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;"> Wishing you & your family a joyful and prosperous Diwali in advance! </p>
+        <p style="font-size: 13px; color: #5e5e5e; text-align: start; margin: 1rem 0 0;">Wishing you & your family a joyful and prosperous Diwali in advance!</p>
       </td>
     </tr>
-    
-    <tr style="padding: 1rem;">
-  <td>
-    <table style="width: 100%; text-align: start; display: flex;">
-      <tr>
-        <td style="padding-bottom: 1rem;">
-          <img src="https://media.licdn.com/dms/image/v2/C4D0BAQH6fJz1s57_eA/company-logo_200_200/company-logo_200_200/0/1630509348990/forwardcode_techstudio_logo?e=1736985600&v=beta&t=nlMSUu3V4zzN6zA9rlbOjdJE7IdnugYYZniJ09UTlNo" 
-          alt="logo" style="width: 90px; height: 90px; background-color: white; margin-bottom: 1rem;">
-        </td>
-      </tr>
-      <tr>
-        <td style="font-size: 14px; font-weight: bold; color: #333;">TEAM HR</td>
-      </tr>
-      <tr>
-        <td style="font-size: 12px; color: #5e5e5e;">Forwardcode TechStudio</td>
-      </tr>
-      <tr>
-        <td style="font-size: 12px; color: #5e5e5e;">Jamshedpur, JH - 831018</td>
-      </tr>
-      <tr>
-        <td style="font-size: 12px; color: #5e5e5e;">
-          <table>
-            <tbody>
-              <tr>
-                <td style="font-size: 12px; color: #5e5e5e;">hr@forwardcode.in</td>
-              </tr>
-              <tr>
-                <td style="padding-top: 1rem;">
-                  <a href="#" style="font-size: 12px; color: #007bff; text-decoration: none;">Check what's new:</a>
-                </td>
-              </tr>
-              <tr>
-                <td style="font-size: 12px; color: #007bff;">https://forwardcode.in</td>
-              </tr>
-            </tbody>
-          </table>
-        </td>
-      </tr>
-    </table>
-  </td>
-</tr>
 
+    <!-- Signature Section -->
+    <tr style="padding: 1rem;" align="start">
+      <td>
+        <table style="width: 100%; text-align: start; display: flex">
+          <tr>
+            <td style="padding-bottom: 1rem;">
+              <img src="https://media.licdn.com/dms/image/v2/C4D0BAQH6fJz1s57_eA/company-logo_200_200/company-logo_200_200/0/1630509348990/forwardcode_techstudio_logo?e=1736985600&v=beta&t=nlMSUu3V4zzN6zA9rlbOjdJE7IdnugYYZniJ09UTlNo" 
+              alt="logo" style="width: 90px; height: 90px; background-color: white; margin-bottom: 1rem;">
+            </td>
+          </tr>
+          <tr>
+          <tr>
+            <td style="font-size: 12px; color: #5e5e5e;">
+              <table>
+                <tbody>
+                  <tr>
+                    <td style="font-size: 12px; color: #5e5e5e;">hr@forwardcode.in</td>
+                  </tr>
+                  <tr>
+                    <td style="padding-top: 1rem;">
+                      <a href="#" style="font-size: 12px; color: #007bff; text-decoration: none;">Check what's new:</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style="font-size: 12px; color: #007bff;">https://forwardcode.in</td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tr>
+        </table>
+      </td>
+    </tr>
 
     <!-- Footer Section -->
     <tr>
       <td align="start" style="padding: 1rem;">
         <p style="font-size: 8px; margin: 0;">PLEASE CONSIDER THE ENVIRONMENT BEFORE PRINTING THIS EMAIL.</p>
-
-        <p style="font-size: 8px; margin: 0;">PLEASE CONSIDER THE ENVIRONMENT BEFORE PRINTING THIS EMAIL.
-          THIS MESSAGE IS INTENDED ONLY FOR THE USE OF THE INDIVIDUAL OR ENTITY TO WHICH IT IS ADDRESSED AND
-          MAY CONTAIN INFORMATION THAT IS PRIVILEGED, CONFIDENTIAL AND EXEMPT FROM DISCLOSURE UNDER APPLICABLE LAW. 
-          IF THE READER OF THIS MESSAGE IS NOT THE INTENDED RECIPIENT, OR THE EMPLOYEE OR AGENT RESPONSIBLE FOR 
-          DELIVERING THE MESSAGE TO THE INTENDED RECIPIENT, YOU ARE HEREBY NOTIFIED THAT ANY DISSEMINATION,
-          DISTRIBUTION OR COPYING OF THIS COMMUNICATION IS STRICTLY PROHIBITED. IF YOU HAVE RECEIVED THIS 
-          COMMUNICATION IN ERROR, PLEASE NOTIFY US IMMEDIATELY BY TELEPHONE AND/OR RETURN E-MAIL. THANK YOU</p>
+        <p style="font-size: 8px; margin: 0;">THIS MESSAGE IS INTENDED ONLY FOR THE USE OF THE INDIVIDUAL OR ENTITY TO WHICH IT IS ADDRESSED AND MAY CONTAIN INFORMATION THAT IS PRIVILEGED, CONFIDENTIAL AND EXEMPT FROM DISCLOSURE UNDER APPLICABLE LAW. IF THE READER OF THIS MESSAGE IS NOT THE INTENDED RECIPIENT, OR THE EMPLOYEE OR AGENT RESPONSIBLE FOR DELIVERING THE MESSAGE TO THE INTENDED RECIPIENT, YOU ARE HEREBY NOTIFIED THAT ANY DISSEMINATION, DISTRIBUTION OR COPYING OF THIS COMMUNICATION IS STRICTLY PROHIBITED. IF YOU HAVE RECEIVED THIS COMMUNICATION IN ERROR, PLEASE NOTIFY US IMMEDIATELY BY TELEPHONE AND/OR RETURN E-MAIL. THANK YOU</p>
       </td>
     </tr>
   </table>
-</body>`,
+</body>
+`,
   };
 
   // Send email
